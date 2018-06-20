91ad466c38669a20e18f9a52be1400dc9795262c
Rhys ILott <daricecake@protonmail.com>
2018-06-20 15:20:11 +0100

    working code

--- Ceasor Cipher/Ceasor_Func.cs
+++ Ceasor Cipher/Ceasor_Func.cs
@@ -17,15 +17,19 @@ namespace Ceasor_Cipher
             return sout;
         }
         public void Dcr (string Word){
+            char[] Outchr = new char[Word.Length];            
             char[] De_Word = new char[Word.Length];
             De_Word=StrToArr(Word);
             int[] castchr = new int[De_Word.Length];
             for(int i=0;i<28;i++){
                 for(int k = 0;k<De_Word.Length;k++){
                     int tempint = De_Word[k] - i;
-                    Console.WriteLine((char)tempint);
+                    Outchr[k]=(char)tempint;
                 }
+            Console.WriteLine(Outchr);    
             }
+                
+            
         }
     }
 }
