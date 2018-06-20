abe56e38dc830ccaf24d997280e38bca040704ab
DaRiceCake <34009985+DaRiceCake@users.noreply.github.com>
2018-06-20 21:16:13 +0100

    works morer

---
+++ Ceasor Cipher/Ceasor Cipher/ConsoleApp1/Ceasor_Funs.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ceasor_Cipher
+{
+    class Ceasor_Funs
+    {
+        private char[] StrToArr(string Word)
+        {
+            int k = 0;
+            char[] sout = new char[Word.Length];
+            foreach (char chr in Word)
+            {
+
+                sout[k] = chr;
+                k = k + 1;
+            }
+            return sout;
+        }
+        public void Dcr(string Word)
+        {
+            char[] Outchr = new char[Word.Length];
+            char[] De_Word = new char[Word.Length];
+            De_Word = StrToArr(Word);
+            int[] castchr = new int[De_Word.Length];
+            for (int i = 0; i < 28; i++)
+            {
+                for (int k = 0; k < De_Word.Length; k++)
+                {
+                    int tempint = De_Word[k] - i;
+                    Outchr[k] = (char)tempint;
+                }
+                Console.WriteLine(Outchr);
+            }
+
+
+        }
+    }
+}
+    
+
--- Ceasor Cipher/Ceasor Cipher/ConsoleApp1/Program.cs
+++ Ceasor Cipher/Ceasor Cipher/ConsoleApp1/Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Ceasor_Cipher;
+
 namespace Ceasor_Cipher
+
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Ceasor_Func func = new Ceasor_Func();
-            func.Dcr("Jack");
+            
         }
     }
 }
