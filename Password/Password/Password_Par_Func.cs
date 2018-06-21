ab3d499a0691c40fc79213c5dd3ebdaa1387ef55
Rhys ILott <daricecake@protonmail.com>
2018-06-22 00:16:46 +0100

    password program beta

--- Password/Password/Password_Par_Func.cs
+++ Password/Password/Password_Par_Func.cs
@@ -7,13 +7,46 @@ namespace Password
 {
     class Password_Par_Func
     {
-        private string password;
-
+        private string password = "test";
         public string Password
         {
             get { return password; }
-        }
+            set
+            {
+                if (value == password)
+                {
+                    password = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
 
+            }
+        }
+        public void PasswordChck()
+        {
+            bool flag2 = false;
+            bool flag = false;
+            while (flag == false)
+            {
+                string att = Console.ReadLine();
+                flag2 = false;
+                try { Password = att; }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine("Leo does not permit");
+                    flag2 = true;
+                }
+                if (flag2 == false)
+                {
+                    flag = true;
+                }
+            }
+            Console.WriteLine("Accepted");
+            
+            
+        }
 
 
     } 
--- Password/Password/Program.cs
+++ Password/Password/Program.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using Password;
 namespace Password
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Password_Par_Func func = new Password_Par_Func();
+            func.PasswordChck();
         }
     }
 }
